f49994b4478ac7f9229f0f203ffffdadce841ad9
Radoslav Ivanov <radogi001@gmail.com>
2022-09-18 10:25:22 +0300

    initial commit

---
+++ 02.LinearDataStructures/QueueImplementation/CoolQueue.cs
@@ -0,0 +1,29 @@
+﻿namespace QueueImplementation
+{
+    class CoolQueue<T>
+    {
+        private ImplementLinkedList.LinkedList<T> linkedList;
+
+        public CoolQueue()
+        {
+            linkedList = new ImplementLinkedList.LinkedList<T>();
+        }
+
+        public int Count { get { return linkedList.Count; } }
+
+        public void Enqueue(T element)
+        {
+            linkedList.AddLast(element);
+        }
+
+        public T Peek()
+        {
+            return linkedList.Head.Value;
+        }
+
+        public T Dequeue()
+        {
+            return linkedList.RemoveHead().Value;
+        }
+    }
+}
---
+++ 02.LinearDataStructures/StackImplementation/CoolStack.cs
@@ -0,0 +1,33 @@
+﻿using ImplementLinkedList;
+using System;
+using System.Text;
+
+namespace StackImplementation
+{
+    class CoolStack<T>
+    {
+        private ImplementLinkedList.LinkedList<T> linkedList;
+
+        public CoolStack()
+        {
+            linkedList = new ImplementLinkedList.LinkedList<T>();
+        }
+
+        public int Count { get { return linkedList.Count; } }
+
+        public void Push(T element)
+        {
+            linkedList.Add(element);
+        }
+
+        public T Peek()
+        {
+            return linkedList.Head.Value;
+        }
+
+        public T Pop()
+        {
+            return linkedList.RemoveHead().Value;
+        }
+    }
+}
